26100ca9badfc5b63d2b66c51abc2e40c7415e97
Yoel Betancor <juyobema@gmail.com>
2024-02-08 13:33:19 +0000

    added functionality of storm moving

--- src/Enviroment.js
+++ src/Enviroment.js
@@ -3,8 +3,12 @@ class Enviroment {
     self = this;
     this.groundSprite;
     this.ground2Sprite;
+    this.cloudsSprite;
+    this.clouds2Sprite
     this.groundLeft;
     this.ground2Left;
+    this.cloudsLeft;
+    this.clouds2Left;
     this.castleLeft;
     this.castleWidht = 50;
     this.castleTop = 30;
@@ -27,12 +31,16 @@ class Enviroment {
 
     this.groundLeft = 0;
     this.ground2Left = 100;
+    this.cloudsLeft = 0;
+    this.clouds2Left = 100;
     this.castleLeft = 110;
     this.progress = 0;
     this.opacity = 1;
     this.isOnCastle = false;
     this.castleSprite = document.getElementById("castle");
     this.castleSprite.style.left = this.castleLeft+"vw"
+    this.cloudsSprite = document.getElementById("storm1");
+    this.clouds2Sprite = document.getElementById("storm2");
     this.groundSprite = document.getElementById("ground");
     this.ground2Sprite = document.getElementById("ground2");
     this.morningBg = document.getElementById("morning");
@@ -52,13 +60,31 @@ class Enviroment {
           this.progress++;
           this.groundLeft--;
           this.ground2Left--;
-          if (this.groundLeft + 100 <= 0) {
+          this.cloudsLeft-=0.3
+          this.clouds2Left-=0.3
+
+          if (this.groundLeft + 100 <= 0  ) {
             this.groundLeft = 99;
+       
           }
 
-          if (this.ground2Left + 100 <= 0) {
+          if (this.ground2Left + 100 <= 0  ) {
             this.ground2Left = 99;
           }
+
+          if (this.cloudsLeft + 100 <= 0  ) {
+            this.cloudsLeft = 99;
+       
+          }
+
+          if (this.clouds2Left + 100 <= 0  ) {
+            this.clouds2Left = 99;
+       
+          }
+
+         
+
+
 
           if (this.progress > 600) {
             this.castleLeft--;
@@ -71,13 +97,28 @@ class Enviroment {
           this.progress--;
           this.groundLeft++;
           this.ground2Left++;
+          this.cloudsLeft+=0.3
+          this.clouds2Left+=0.3
 
-          if (this.groundLeft >= 100) {
+          if (this.groundLeft >= 100  ) {
             this.groundLeft = -100 + 1;
+           
           }
           
-          if (this.ground2Left >= 100) {
+          if (this.ground2Left >= 100  ) {
             this.ground2Left = -100 + 1;
+             
+          }
+
+
+          if (this.cloudsLeft  >= 100  ) {
+            this.cloudsLeft = -99;
+       
+          }
+
+          if (this.clouds2Left  >= 100  ) {
+            this.clouds2Left = -99;
+       
           }
 
           if (this.progress > 600) {
@@ -89,6 +130,9 @@ class Enviroment {
 
         this.groundSprite.style.left = this.groundLeft + "vw";
         this.ground2Sprite.style.left = this.ground2Left + "vw";
+        this.cloudsSprite.style.left = this.cloudsLeft + "vw";
+        this.clouds2Sprite.style.left = this.clouds2Left + "vw";
+
       }
 
       this.checkCollisions();
